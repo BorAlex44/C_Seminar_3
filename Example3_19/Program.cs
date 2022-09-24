260396a9c7705f17b3ced1b345d238c7770b6f6a
BorAlex <alexander44@bl.ru>
2022-09-24 17:36:35 +0300

    add Example3_19

--- Example3_19/Program.cs
+++ Example3_19/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+WriteLine("Введите любое пятизначное число: ");
+int number = int.Parse(ReadLine()!);
+int temp1 = number / 1000;
+int temp2 = number % 100;
+if ((temp1 / 10 == temp2 % 10) && (temp1 % 10 == temp2 / 10))
+{
+    WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+    WriteLine($"Число {number} не является палиндромом");
+}
