f14b11df064af8066d3c3afe3e9c5a677927e794
BorAlex <alexander44@bl.ru>
2022-09-24 17:43:49 +0300

    add Example3_19 version 2

---
+++ Exmple3_19v2/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите любое пятизначное число: ");
+int number = int.Parse(ReadLine()!);
+if (((number / 1000) / 10 == (number % 100) % 10) && ((number / 1000) % 10 == (number % 100) / 10))
+{
+    WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+    WriteLine($"Число {number} не является палиндромом");
+}
